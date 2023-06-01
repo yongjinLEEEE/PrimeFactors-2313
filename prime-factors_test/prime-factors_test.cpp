5cdc34d6f7184d66507f1f71e51e18c2ceec0342
yongjin lee <vir2048@gmail.com>
2023-06-01 15:59:45 +0900

    apply primefactor.
    Signed-off-by: yongjin lee <vir2048@gmail.com>

--- prime-factors_test/prime-factors_test.cpp
+++ prime-factors_test/prime-factors_test.cpp
@@ -1,6 +1,47 @@
 #include "pch.h"
+#include "../Project9/prime-factors.cpp"
+#include <vector>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+class PrimeFixture : public testing::Test
+{
+public:
+	PrimeFactor prime_factor;
+	vector<int> expected;
+};
+TEST_F(PrimeFixture, of1) {
+	
+	expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST_F(PrimeFixture, of2) {
+	expected = { 2 };
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+
+TEST_F(PrimeFixture, of3) {
+	expected = { 3 };
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
+
+TEST_F(PrimeFixture, of4) {
+	expected = { 2, 2 };
+	EXPECT_EQ(expected, prime_factor.of(4));
+}
+
+TEST_F(PrimeFixture, of6) {
+	expected = { 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
+
+TEST_F(PrimeFixture, of9) {
+	expected = { 3, 3 };
+	EXPECT_EQ(expected, prime_factor.of(9));
+}
+
+TEST_F(PrimeFixture, of12) {
+	expected = { 2, 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(12));
 }
